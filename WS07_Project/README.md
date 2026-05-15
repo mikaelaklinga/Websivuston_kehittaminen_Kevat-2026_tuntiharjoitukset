6a43f877c5022ef9dfa2b6b7baa912f9b4833ea2
Mikaela Klinga <mik00534@laurea.fi>
2026-05-15 20:13:44 +0300

    Refactor palvelut and tietoa pages: Update CSS styles for service cards, improve layout and responsiveness, and enhance HTML structure for better accessibility and readability. Add new sections for service details and personal introduction.

--- WS07_Project/README.md
+++ WS07_Project/README.md
@@ -1,1 +1,13 @@
-TÄmä on työpaja WS07 kansio.
+Tämä on kurssin viimeinen tehtävä: Projektityö verkkosivut
+Tein verkkosivut oikealle yritykselle. 
+Sivulla on neljä sivua: Etusivu, Palvelut, Tietoa, Ajanvaraus 
+
+Sivusto on suunniteltu ja määritelty niin että tämän tehtäväpalautuksen jälkeen suoritamme domain hankinnat, ohjaamme "ota yhteyttä" lomakkeen toimminnon Formspreen kautta suoraan yhteydenoton sähköpostiin, ilman että shäköpostia tarvitsee uudelleen kirjoittaa ja verkkosivu julkaistaan myös virallisesti kaikille. 
+
+Nyt olen tehnyt verkkosivun siihen pisteeseen valmiiksi kuin tämä tehtävänanto sitä edellyttää. 
+
+Olen käyttänyt tehtävää tehdessäni tekoälyä apunani.
+    - Tekoäly Mistral AI, Le Chat tekoälyavustajaa (think) toiminnolla 
+    - Tekoäly Chat GPT:tä
+
+Muut lähteet löytyvät raportin lopusta. 
--- WS07_Project/main.js
+++ WS07_Project/main.js
@@ -1,8 +1,20 @@
-function toggleHamburger() {  
-  const nav = document.getElementById("mainNav");
-  if (nav.className === "topnav") {
-    nav.className += " responsive";
-  } else {
-    nav.className = "topnav";
-  }
-} 
+// Tällä lisätään tai poistetaan luokka (scrolled) header-elementistä käyttäjän skrollatessa sivua.
+    window.addEventListener('scroll', function() {
+      const header = document.getElementById('header');
+      if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    });
+
+// Mobiilivalikon toggle, mahdollistaa valikon avaamisen ja sulkemisen mobiililaitteella (niinkuin tässä: hamburger-ikonista ristiin).
+    function toggleMenu() {
+      const menu = document.getElementById('mobileMenu');
+      const menuIcon = document.querySelector('.menu-icon');
+      const closeIcon = document.querySelector('.close-icon');
+      
+      menu.classList.toggle('open');
+      menuIcon.classList.toggle('hidden');
+      closeIcon.classList.toggle('hidden');
+    }
